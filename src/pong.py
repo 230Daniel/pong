177e23963822fa16fe23fc39765959b72cf04430
Daniel Baynton <daniel.baynton@hotmail.com>
2021-06-16 13:15:12 +0100

    *throws up some more python code*

--- src/pong.py
+++ src/pong.py
@@ -11,14 +11,17 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Pong")
 clock = pg.time.Clock()
 
+ballImage = pg.image.load("Game/ball.png").convert_alpha()
+ballImage = pg.transform.smoothscale(ballImage, (80, 80))
+
 player = Player(WIDTH, HEIGHT)
-ball = Ball(WIDTH, HEIGHT)
+ball = Ball(WIDTH, HEIGHT, ballImage)
 
 all_sprites = pg.sprite.Group()
 all_sprites.add(player)
-all_sprites.add(ball)
 
 running = True
+
 while running:
 
 	clock.tick(FPS)
@@ -32,7 +35,7 @@ while running:
 				all_sprites = pg.sprite.Group()
 				player = Player(WIDTH, HEIGHT)
 				all_sprites.add(player)
-				ball = Ball(WIDTH, HEIGHT)
+				ball = Ball(WIDTH, HEIGHT, ballImage)
 				all_sprites.add(ball)
 
 	if ball.game_over:
@@ -43,12 +46,13 @@ while running:
 		ball.update()
 
 		if pg.sprite.collide_rect(player, ball):
-			ball.collidedWithPadel()
+			ball.collidedWithPaddle()
 
 		screen.fill((0, 0, 0))
 
 		for sprite in all_sprites:
 			screen.blit(sprite.surf, sprite.rect)
+		screen.blit(ball.image, ball.rect)
 
 	pg.display.flip()       
 
